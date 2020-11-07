8728f7655705d4f4e18276a2cbcbecd0955b8ab0
Blake Lampkin <blakequake@BlakeQuakes-UFO.lan>
2020-11-06 19:38:25 -0800

    got the basics working

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import logo from './logo.svg';
+import About from "./pages/about/about";
+import './App.css';
+
+
+
+function App() {
+  return (
+    <>
+    <div className="App">
+        <About>
+
+        </About>
+    </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/about/about.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./about.css";
+
+export default () => {  
+        return ( 
+        <div> 
+            <h1>Hello Blake</h1>
+
+        </div>
+)}
